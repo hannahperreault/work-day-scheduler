0df16cb9f406e9bf91148c0e4c89f3d58427de5b
Hannah Perreault <hperre@gmail.com>
2020-05-03 11:43:55 -0400

    color coding works for each hour

--- develop/script.js
+++ develop/script.js
@@ -1,8 +1,23 @@
 //need to have day field added
 //variable for currentDate
-//append to jumbotron: $("#current-day").append(currentDate);
+var currentDate = moment().format("dddd" + "," + " MMM Do");
+$("#current-day").append("Today is: " + currentDate);
 
-//variate for currentTime
+//variable for currentTime
+var currentTime = moment().format("HH");
+console.log(currentTime);
+
+//function to color code based on whether time block should be labeled as past, present, or future, in relation to current time
+$(".text-field").each(function () {
+  var hourValue = $(this).attr("id");
+  console.log(hourValue);
+  if (hourValue < currentTime) {
+    $(this).attr("class", "text-field form-control past");
+  } else if (hourValue === currentTime) {
+    $(this).attr("class", "text-field form-control present");
+  }
+  console.log(this);
+});
 
 //create if statement
 //if time is earlier than currentTime, then add class .past to input class on text field
