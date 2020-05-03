09a3c96a3cf99019c9a0b4e8d0b9c910a0bca80f
Hannah Perreault <hperre@gmail.com>
2020-05-03 13:06:22 -0400

    local storage for each input field

--- develop/script.js
+++ develop/script.js
@@ -7,22 +7,92 @@ $("#current-day").append("Today is: " + currentDate);
 var currentTime = moment().format("HH");
 console.log(currentTime);
 
+$(window).on("unload", function () {
+  loadSettings();
+});
+
+function loadSettings() {
+  var displayValue = localStorage.getItem("#10", value);
+  console.log(displayValue);
+  $("#10").push(displayValue);
+  //   if (value !== null) $("#inputName").val(value);
+}
+
 //function to color code based on whether time block should be labeled as past, present, or future, in relation to current time
 $(".text-field").each(function () {
   var hourValue = $(this).attr("id");
-  console.log(hourValue);
   if (hourValue < currentTime) {
     $(this).attr("class", "text-field form-control past");
   } else if (hourValue === currentTime) {
     $(this).attr("class", "text-field form-control present");
   }
-  console.log(this);
 });
 
-//create if statement
-//if time is earlier than currentTime, then add class .past to input class on text field
-//if time is in range of current hour, then add class .present to input class on text field
-//if time is in in future hour, add class .future to input class on text field
+//saving text input to local storage. I know this could be dry, but can't figure out how..
+$(document).ready(function () {
+  $("#button-save-9").click(function () {
+    var value = $("#09").val();
+    console.log(value);
+    localStorage.setItem("#09", value);
+  });
+});
+
+$(document).ready(function () {
+  $("#button-save-10").click(function () {
+    var value = $("#10").val();
+    console.log(value);
+    localStorage.setItem("#10", value);
+  });
+});
+$(document).ready(function () {
+  $("#button-save-11").click(function () {
+    var value = $("#11").val();
+    console.log(value);
+    localStorage.setItem("#11", value);
+  });
+});
+$(document).ready(function () {
+  $("#button-save-12").click(function () {
+    var value = $("#12").val();
+    console.log(value);
+    localStorage.setItem("#12", value);
+  });
+});
+$(document).ready(function () {
+  $("#button-save-13").click(function () {
+    var value = $("#13").val();
+    console.log(value);
+    localStorage.setItem("#13", value);
+  });
+});
+$(document).ready(function () {
+  $("#button-save-14").click(function () {
+    var value = $("#14").val();
+    console.log(value);
+    localStorage.setItem("#14", value);
+  });
+});
+$(document).ready(function () {
+  $("#button-save-15").click(function () {
+    var value = $("#15").val();
+    console.log(value);
+    localStorage.setItem("#15", value);
+  });
+});
+$(document).ready(function () {
+  $("#button-save-16").click(function () {
+    var value = $("#16").val();
+    console.log(value);
+    localStorage.setItem("#16", value);
+  });
+});
+$(document).ready(function () {
+  $("#button-save-17").click(function () {
+    var value = $("#17").val();
+    console.log(value);
+    localStorage.setItem("#17", value);
+  });
+});
 
 //when save button clicked:
 //event listener needed
