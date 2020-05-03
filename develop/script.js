cccdd5f5464286d0eea3f6c94ef668d2db375e28
Hannah Perreault <hperre@gmail.com>
2020-05-03 09:49:46 -0400

    pseudocode on script.js

---
+++ develop/script.js
@@ -0,0 +1,16 @@
+//need to have day field added
+//variable for currentDate
+//append to jumbotron: $("#current-day").append(currentDate);
+
+//variate for currentTime
+
+//create if statement
+//if time is earlier than currentTime, then add class .past to input class on text field
+//if time is in range of current hour, then add class .present to input class on text field
+//if time is in in future hour, add class .future to input class on text field
+
+//when save button clicked:
+//event listener needed
+//add text field to local storage
+//clear auto formatting
+//keep in text field upon restoring page
